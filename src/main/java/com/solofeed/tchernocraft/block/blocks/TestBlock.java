b19f9ccb7521e15e7f0fe6509b19fd0156a6951a
Solofeed <louismarie.guillemot@gmail.com>
2017-05-10 23:33:12 +0200

    add : ajout property facing sur le test block

--- src/main/java/com/solofeed/tchernocraft/block/blocks/TestBlock.java
+++ src/main/java/com/solofeed/tchernocraft/block/blocks/TestBlock.java
@@ -7,38 +7,44 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemMultiTexture;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by Solofeed on 07/05/2017.
  * For testing purpose
  */
 @TchernocraftBlock
-public class TestTchernocraftBlock extends Block implements ITchernocraftBlockWithProperties {
+public class TestBlock extends Block implements ITchernocraftBlockWithProperties {
     public final static String NAME = "test_block";
 
     private final static Material MATERIAL = Material.ROCK;
     private final static IProperty<EnumType> TYPE = PropertyEnum.create("type", EnumType.class);
+    private final static PropertyDirection FACING = PropertyDirection.create("facing");
 
-    public TestTchernocraftBlock() {
+    public TestBlock() {
         super(MATERIAL);
         setSoundType(SoundType.METAL);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(TYPE, EnumType.VARIANT_A));
+        IBlockState state = this.blockState.getBaseState()
+                .withProperty(TYPE, EnumType.VARIANT_A)
+                .withProperty(FACING, EnumFacing.NORTH);
+        this.setDefaultState(state);
     }
 
     @Override
@@ -46,30 +52,37 @@ public class TestTchernocraftBlock extends Block implements ITchernocraftBlockWi
         return NAME;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
-    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
-        super.onBlockAdded(worldIn, pos, state);
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState()
+                .withProperty(FACING, EnumFacing.getDirectionFromEntityLiving(pos, placer))
+                .withProperty(TYPE, getStateFromMeta(meta * EnumFacing.values().length).getValue(TYPE));
     }
 
     @Override
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[]{TYPE});
+        return new BlockStateContainer(this, new IProperty[]{TYPE, FACING});
     }
 
     @Override
     public int getMetaFromState(IBlockState state) {
-        return state.getValue(TYPE).getMeta();
+        int typeMeta = state.getValue(TYPE).getMeta();
+        int facingMeta = state.getValue(FACING).ordinal();
+        return typeMeta * EnumFacing.values().length + facingMeta; //Stores the type the EnumFacing in the meta
     }
 
     @SuppressWarnings("deprecation")
     @Override
     public IBlockState getStateFromMeta(int meta) {
-        return getDefaultState().withProperty(TYPE, EnumType.values()[meta]);
+        EnumType type = EnumType.values()[(meta / EnumFacing.values().length) % EnumType.values().length]; //Gets the type from the meta
+        EnumFacing facing = EnumFacing.values()[meta % EnumFacing.values().length]; //Gets the EnumFacing from the meta
+        return this.getDefaultState().withProperty(TYPE, type).withProperty(FACING, facing); //Returns the correct state
     }
 
     @Override
     public int damageDropped(IBlockState state) {
-        return getMetaFromState(state);
+        return (getMetaFromState(state) / EnumFacing.values().length);
     }
 
     @Override
